170a37aefd5f1448b20123884f466422693a0851
Karolass <tark810145@gmail.com>
2019-01-19 11:36:46 +0800

    補一些sticker

--- controllers/bot.js
+++ controllers/bot.js
@@ -13,6 +13,20 @@ const ifText = async (chatId, text) => {
     await request.sendSticker(chatId, 'CAADBQADCAIAAhZUFRUNgHEHih2ysQI')
   } else if (/喬瑟夫/i.test(text)) {
     await request.sendAnimation(chatId, 'CgADBQADRwAD6gQQVfERILdPn8_zAg')
+  } else if (/不是吧/i.test(text)) {
+    await request.sendSticker(chatId, 'CAADBQAD9AEAAhZUFRUgCTdacKLTkQI')
+  } else if (/求包養/i.test(text)) {
+    await request.sendSticker(chatId, 'CAADBQAD_AEAAhZUFRV0woJ6SekjNQI')
+  } else if (/有病/i.test(text)) {
+    await request.sendSticker(chatId, 'CAADBQAD-wEAAhZUFRVE_IjYqIcvQAI')
+  } else if (/興奮/i.test(text)) {
+    await request.sendSticker(chatId, 'CAADBQADCgIAAhZUFRV_Hv6tJvdJUQI')
+  } else if (/幫QQ/i.test(text)) {
+    await request.sendSticker(chatId, 'CAADBQADBwIAAhZUFRVDo07nzoPQbgI')
+  } else if (/甲甲/i.test(text)) {
+    await request.sendSticker(chatId, 'CAADBQADBAIAAhZUFRXTMnOJyUjC3wI')
+  } else if (/屌/i.test(text)) {
+    await request.sendSticker(chatId, 'CAADBQADDQIAAhZUFRUN4_JgD3TfewI')
   }
 }
 
